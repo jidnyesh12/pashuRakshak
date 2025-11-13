8995dcc4e9669fe7f01dfe2eb7cbcada7adec2c1
Jidnyesh <eclipse-clip-gusty@duck.com>
2025-11-13 13:49:19 +0530

    Initial Project Setup

---
+++ pashuRakshak-backend/src/main/java/com/example/pashuRakshak/PashuRakshakApplication.java
@@ -0,0 +1,13 @@
+package com.example.pashuRakshak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PashuRakshakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PashuRakshakApplication.class, args);
+	}
+
+}
---
+++ pashuRakshak-backend/src/test/java/com/example/pashuRakshak/PashuRakshakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pashuRakshak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PashuRakshakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
